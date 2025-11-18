dc769df7b0e7f9c7ece6f210bbc4e33b3e2221f3
Zunaira3 <zunairak4321@gmail.com>
2025-11-18 11:43:19 +0500

    Add files via upload

---
+++ B19_03_3.m
@@ -0,0 +1,170 @@
+%Qs No:3
+%reading an image
+F=imread('cameraman.png');
+F=im2double(F);
+%seperating R G B channels
+r=F(:,:,1);
+g=F(:,:,2);
+b=F(:,:,3);
+%Converting to HSI using conversion formulas
+th=acos((0.5*((r-g)+(r-b)))./((sqrt((r-g).^2+(r-b).*(g-b)))+eps));
+H=th;
+H(b>g)=2*pi-H(b>g);
+H=H/(2*pi);
+S=1-3.*(min(min(r,g),b))./(r+g+b+eps);
+V=(r+g+b)/3;
+HSI=cat(3,H,S,V);
+%Applying filter h1 and h2 on the I channel of HSI space
+image = im2double(V);
+%declaring avg flter 3*3
+B = [ 1 1 1;1 1 1; 1 1 1];
+%Finding size of image
+[r,c] = size (image);
+filt_I_h1 = zeros(r,c);
+for i = 2:r-1
+    for j = 2:c-1
+        Im1 = [image(i-1,j-1) image(i-1,j) image(i-1,j+1) ; image(i,j-1) image(i,j) image(i,j+1) ; image(i+1,j-1) image(i+1,j) image(i+1,j+1)];
+        IM2 = Im1.*B;
+        IM3=IM2/9;
+        %avg(i,j)=sum(temp2)/9;
+        
+        filt_I_h1(i,j) = IM3(1,1) + IM3(1,2) + IM3(1,3) + IM3(2,1) + IM3(2,2) + IM3(2,3) +  IM3(3,1) + IM3(3,2) + IM3(3,3);
+        
+    end
+end
+%applying h2 filter on I channel
+C = [ 1 4 6 4 1 ;4 16 24 16 4;6 24 36 24 6;4 16 24 16 4; 1 4 6 4 1];
+%getting size of image 
+[r,c] = size (image);
+filt_I_h2 = zeros(r,c);
+
+for i = 3:r-2
+    for j = 3:c-2
+        V1 = [image(i-2,j-2) image(i-2,j-1) image(i-2,j) image(i-2,j+1) image(i-2,j+2); image(i-1,j-2) image(i-1,j-1) image(i-1,j) image(i-1,j+1) image(i-1,j+2) ; image(i,j-2) image(i,j-1) image(i,j) image(i,j+1) image(i,j+2);image(i+1,j-2) image(i+1,j-1) image(i+1,j) image(i+1,j+1) image(i+1,j+2); image(i+2,j-2) image(i+2,j-1) image(i+2,j) image(i+2,j+1) image(i+2,j+2) ];
+        I2 = V1.*C;
+        I3=I2/256;
+
+        
+        filt_I_h2(i,j) = I3(1,1) + I3(1,2) + I3(1,3)+I3(1,4) + I3(1,5) + I3(2,1) + I3(2,2) + I3(2,3)+I3(2,4) + I3(2,5) +  I3(3,1) + I3(3,2) + I3(3,3)+I3(3,4) + I3(3,5);
+    end
+end
+
+%Making an hsi image with H and S same and I as filtered one
+hsi=cat(3,H,S,filt_I_h1);
+hsi1=cat(3,H,S,filt_I_h2);
+C=hsitorgb(hsi);
+D=hsitorgb(hsi1);
+
+figure(1)
+subplot 221
+imshow(hsi)
+title('HSI Image with I channel Filtered by h1');
+subplot 222
+imshow(hsi1)
+title('HSI Image with I channel Filtered by h2');
+subplot 223
+imshow(C)
+title('RGB Image with I channel Filtered by h1');
+subplot 224
+imshow(D)
+title('RGB Image with I channel Filtered by h2');
+
+[N1,N2] = size (image);
+%MSE for h1 filtered image
+Im=(F-C).^2;
+i1=Im(:,:,1);
+i2=Im(:,:,2);
+i3=Im(:,:,3);
+S1=sum(i1,'all');
+S2=sum(i2,'all');
+S3=sum(i3,'all');
+Avg=(S1+S2+S3)/3;
+%S=sum(D(:));
+E=1/(N1*N2);
+MSE=E.*Avg;
+Max=255^2;
+disp("PSNR for image with h1 filter :")
+PSNR=10*log10(Max/MSE);
+disp(PSNR)
+
+% %MSE for h2 filterd image
+
+Im=(F-D).^2;
+i1=Im(:,:,1);
+i2=Im(:,:,2);
+i3=Im(:,:,3);
+S1=sum(i1,'all');
+S2=sum(i2,'all');
+S3=sum(i3,'all');
+Avg1=(S1+S2+S3)/3;
+E=1/(N1*N2);
+MSE1=E.*Avg1;
+
+Max=255^2;
+PSNR1=10*log10(Max/MSE1);
+disp("PSNR for image with h2 filter :")
+disp(PSNR1)
+
+%Function to convert back to RGB space
+function C=hsitorgb(hsi)
+
+HV=hsi(:,:,1)*2*pi;
+
+SV=hsi(:,:,2);
+
+IV=hsi(:,:,3);
+
+R=zeros(size(HV));
+
+G=zeros(size(HV));
+
+B=zeros(size(HV));
+
+%RG Sector
+
+id=find((0<=HV)& (HV<2*pi/3));
+
+B(id)=IV(id).*(1-SV(id));
+
+R(id)=IV(id).*(1+SV(id).*cos(HV(id))./cos(pi/3-HV(id)));
+
+G(id)=3*IV(id)-(R(id)+B(id));
+
+%BG Sector
+
+id=find((2*pi/3<=HV)& (HV<4*pi/3));
+
+R(id)=IV(id).*(1-SV(id));
+
+G(id)=IV(id).*(1+SV(id).*cos(HV(id)-2*pi/3)./cos(pi-HV(id)));
+
+B(id)=3*IV(id)-(R(id)+G(id));
+
+%BR Sector
+
+id=find((4*pi/3<=HV)& (HV<2*pi));
+
+G(id)=IV(id).*(1-SV(id));
+
+B(id)=IV(id).*(1+SV(id).*cos(HV(id)-4*pi/3)./cos(5*pi/3-HV(id)));
+
+R(id)=3*IV(id)-(G(id)+B(id));
+
+C=cat(3,R,G,B);
+
+C=max(min(C,1),0);
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ B19_03_4.m
@@ -0,0 +1,172 @@
+%Question 1 and 2 implemented on SKYLINE image
+clear all
+close all
+%Reading Image 
+A=imread('skyline.jpg');
+A=im2double(A);
+%Seperating red green  and blue channels
+Red=A(:,:,1);
+Green=A(:,:,2);
+Blue=A(:,:,3);
+%plotting image 
+figure (1)
+subplot 221
+imshow(A)
+title('Cameraman Image')
+%Displaying red channel
+subplot 222
+imshow(Red)
+title('Red Channel')
+%Displaying green channel
+subplot 223
+imshow(Green)
+title('Green Channel')
+%Displaying blue channel
+subplot 224
+imshow(Blue)
+title('Blue Channel')
+
+%Converting image to greyscale
+
+image = im2double(Red);
+%declaring avg flter 3*3
+B = [ 1 1 1;1 1 1; 1 1 1];
+%Finding size of image
+[r,c] = size (image);
+IMG = zeros(r,c);
+for i = 2:r-1
+    for j = 2:c-1
+        Im1 = [image(i-1,j-1) image(i-1,j) image(i-1,j+1) ; image(i,j-1) image(i,j) image(i,j+1) ; image(i+1,j-1) image(i+1,j) image(i+1,j+1)];
+        IM2 = Im1.*B;
+        IM3=IM2/9;
+        IMG(i,j) = IM3(1,1) + IM3(1,2) + IM3(1,3) + IM3(2,1) + IM3(2,2) + IM3(2,3) +  IM3(3,1) + IM3(3,2) + IM3(3,3);
+        
+    end
+end
+image1 = im2double(Green);
+%declaring avg flter 3*3
+B = [ 1 1 1;1 1 1; 1 1 1];
+%Finding size of image
+[r,c] = size (image1);
+IMG1 = zeros(r,c);
+for i = 2:r-1
+    for j = 2:c-1
+        Im1 = [image1(i-1,j-1) image1(i-1,j) image1(i-1,j+1) ; image1(i,j-1) image1(i,j) image1(i,j+1) ; image1(i+1,j-1) image1(i+1,j) image1(i+1,j+1)];
+        IM2 = Im1.*B;
+        IM3=IM2/9;
+        IMG1(i,j) = IM3(1,1) + IM3(1,2) + IM3(1,3) + IM3(2,1) + IM3(2,2) + IM3(2,3) +  IM3(3,1) + IM3(3,2) + IM3(3,3);
+        
+    end
+end
+image2 = im2double(Blue);
+%declaring avg flter 3*3
+B = [ 1 1 1;1 1 1; 1 1 1];
+%Finding size of image
+[r,c] = size (image2);
+IMG2 = zeros(r,c);
+for i = 2:r-1
+    for j = 2:c-1
+        Im1 = [image2(i-1,j-1) image2(i-1,j) image2(i-1,j+1) ; image2(i,j-1) image2(i,j) image2(i,j+1) ; image2(i+1,j-1) image2(i+1,j) image2(i+1,j+1)];
+        IM2 = Im1.*B;
+        IM3=IM2/9;
+        IMG2(i,j) = IM3(1,1) + IM3(1,2) + IM3(1,3) + IM3(2,1) + IM3(2,2) + IM3(2,3) +  IM3(3,1) + IM3(3,2) + IM3(3,3);
+        
+    end
+end
+h1=[];
+h1(:,:,1)=IMG;
+h1(:,:,2)=IMG1;
+h1(:,:,3)=IMG2;
+
+
+h1=cat(3,IMG,IMG1,IMG2);
+C = [ 1 4 6 4 1 ;4 16 24 16 4;6 24 36 24 6;4 16 24 16 4; 1 4 6 4 1];
+[r,c] = size (Red);
+Img = zeros(r,c);
+Im = im2double(Red);
+for i = 3:r-2
+    for j = 3:c-2
+        i1 = [Im(i-2,j-2) Im(i-2,j-1) Im(i-2,j) Im(i-2,j+1) Im(i-2,j+2); Im(i-1,j-2) Im(i-1,j-1) Im(i-1,j) Im(i-1,j+1) Im(i-1,j+2) ; Im(i,j-2) Im(i,j-1) Im(i,j) Im(i,j+1) Im(i,j+2);Im(i+1,j-2) Im(i+1,j-1) Im(i+1,j) Im(i+1,j+1) Im(i+1,j+2); Im(i+2,j-2) Im(i+2,j-1) Im(i+2,j) Im(i+2,j+1) Im(i+2,j+2) ];
+        i2 = i1.*C;
+        i3=i2/256;
+        Img(i,j) = i3(1,1) + i3(1,2) + i3(1,3)+i3(1,4) + i3(1,5) + i3(2,1) + i3(2,2) + i3(2,3)+i3(2,4) + i3(2,5) +  i3(3,1) + i3(3,2) + i3(3,3)+i3(3,4) + i3(3,5);
+    end
+end
+
+[m,n] = size (Green);
+X = zeros(m,n);
+X1 = im2double(Green);
+for i = 3:r-2
+    for j = 3:c-2
+        Y = [X1(i-2,j-2) X1(i-2,j-1) X1(i-2,j) X1(i-2,j+1) X1(i-2,j+2); X1(i-1,j-2) X1(i-1,j-1) X1(i-1,j) X1(i-1,j+1) X1(i-1,j+2) ; X1(i,j-2) X1(i,j-1) X1(i,j) X1(i,j+1) X1(i,j+2);X1(i+1,j-2) X1(i+1,j-1) X1(i+1,j) X1(i+1,j+1) X1(i+1,j+2); X1(i+2,j-2) X1(i+2,j-1) X1(i+2,j) X1(i+2,j+1) X1(i+2,j+2) ];
+        X2 = Y.*C;
+        X3=X2/256;
+        X(i,j) = X3(1,1) + X3(1,2) + X3(1,3)+X3(1,4) + X3(1,5) + X3(2,1) + X3(2,2) + X3(2,3)+X3(2,4) + X3(2,5) +  X3(3,1) + X3(3,2) + X3(3,3)+X3(3,4) + X3(3,5);
+    end
+end
+
+
+[y,z] = size (Blue);
+S = zeros(y,z);
+S1 = im2double(Blue);
+for i = 3:r-2
+    for j = 3:c-2
+        Y1 = [S1(i-2,j-2) S1(i-2,j-1) S1(i-2,j) S1(i-2,j+1) S1(i-2,j+2); S1(i-1,j-2) S1(i-1,j-1) S1(i-1,j) S1(i-1,j+1) S1(i-1,j+2) ; S1(i,j-2) S1(i,j-1) S1(i,j) S1(i,j+1) S1(i,j+2);S1(i+1,j-2) S1(i+1,j-1) S1(i+1,j) S1(i+1,j+1) S1(i+1,j+2); S1(i+2,j-2) S1(i+2,j-1) S1(i+2,j) S1(i+2,j+1) S1(i+2,j+2) ];
+        S2 = Y1.*C;
+        S3=S2/256;
+        S(i,j) = S3(1,1) + S3(1,2) + S3(1,3)+S3(1,4) + S3(1,5) + S3(2,1) + S3(2,2) + S3(2,3)+S3(2,4) + S3(2,5) +  S3(3,1) + S3(3,2) + S3(3,3)+S3(3,4) + S3(3,5);
+    end
+end
+h2=cat(3,Img,X,S);
+figure(2)
+subplot 131
+imshow(A)
+title('Original image')
+subplot 132
+imshow(h1)
+title('After applying h1 filter')
+subplot 133
+imshow(h2)
+title('After applying h2 filter')
+
+[N1, N2]=size(A);
+%MSE for h1 filtered image
+Im=(A-h1).^2;
+i1=Im(:,:,1);
+i2=Im(:,:,2);
+i3=Im(:,:,3);
+S1=sum(i1,'all');
+S2=sum(i2,'all');
+S3=sum(i3,'all');
+Avg=(S1+S2+S3)/3;
+%S=sum(D(:));
+E=1/(N1*N2);
+MSE=E.*Avg;
+
+Max=255^2;
+disp("PSNR for image with h1 filter :")
+PSNR=10*log10(Max/MSE);
+disp(PSNR)
+% %MSE for h2 filterd image
+
+Im=(A-h2).^2;
+i1=Im(:,:,1);
+i2=Im(:,:,2);
+i3=Im(:,:,3);
+S1=sum(i1,'all');
+S2=sum(i2,'all');
+S3=sum(i3,'all');
+Avg1=(S1+S2+S3)/3;
+E=1/(N1*N2);
+MSE1=E.*Avg1;
+
+Max=255^2;
+PSNR1=10*log10(Max/MSE1);
+disp("PSNR for image with h2 filter :")
+disp(PSNR1)
+
+
+
+
+
+
